6defa385b8c971e19a21bdb883ed794100e67d5b
AdelyaKrasnova <adelya-nagimova@yandex.ru>
2023-11-14 14:40:27 +0300

    Форматирование кода

--- test_hw2.py
+++ test_hw2.py
@@ -2,6 +2,7 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+
 @pytest.fixture()
 def open_browser():
     browser.config.window_width = 375
@@ -10,10 +11,12 @@ def open_browser():
     yield
     browser.quit()
 
+
 def test_first_search(open_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_second_search(open_browser):
     browser.open('https://google.com')
